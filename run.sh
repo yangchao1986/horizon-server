40f2f44b8ce0ff98d640539b32835980f6a5d697
yangchao1986 <yangchao_x985@163.com>
2022-07-29 11:48:30 +0800

    msg

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ cd $WORK_PATH
 echo '创建镜像'
 docker build -t horizonsys/server . 
 echo '删除旧容器'
-exist=`docker inspect --format '{{.State.Running}}' ${horizonsys-server}`
-if [ "$exist" ];then
-docker stop horizonsys-server
-docker rm horizonsys-server
+exist=`docker inspect --format '{{.State.Running}}' horizon-server`
+if [ "$exist" ];
+then
+docker stop horizon-server
+docker rm horizon-server
 fi;
 echo '创建新容器'
-docker docker run -di --name=horizonsys-server -p 38989:3000 horizonsys/server:v1.0
+docker run -di --name=horizonsys-server -p 38989:3000 horizonsys/server:v1.0
