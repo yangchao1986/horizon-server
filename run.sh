41b0e436daf0652f2a667b0ef56fc1926bf51581
yangchao1986 <yangchao_x985@163.com>
2022-07-29 10:45:47 +0800

    msg

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #！/bin/bash
 WORK_PATH='./server'
 cd $WORK_PATH
-echo '清除旧代码'
-git reset --hard origin/master
-git clean -f
-echo '拉取新代码'
-git pull origin master
-echo '开始构建...'
-docker build -t horizonsys/server .
+echo '创建镜像'
+docker build -t horizonsys/server . 
+echo '删除旧容器'
+exist=`docker inspect --format '{{.State.Running}}' ${horizonsys-server}`
+if [ "$exist" ];then
+docker stop horizonsys-server
+docker rm horizonsys-server
+fi;
+echo '创建新容器'
+docker docker run -di --name=horizonsys-server -p 30080:80 horizonsys/server:v1.0
